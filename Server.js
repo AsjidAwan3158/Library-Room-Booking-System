f464ab633dbb69769420555f25c2003a7a4da225
AsjidAwan3158 <asjidawan3103@gmail.com>
2026-01-17 20:56:09 +0500

    restrustured complete system

--- Server.js
+++ Server.js
@@ -3,8 +3,22 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 console.log("🔥 server.js file is being executed");
+app.use(express.static(path.join(__dirname, "public")));
+
+//pages
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "LibraryBookingSystem.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "LibraryBookingAdmin.html"));
+});
+
 
 const { testConnection } = require("./config/database");
 
@@ -15,8 +29,8 @@ const adminRoutes = require("./routes/admin");
 // const userRoutes = require("./routes/users");
 const testRoutes = require("./routes/testdb");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+
+
 
 /* =======================
    GLOBAL MIDDLEWARE
@@ -32,6 +46,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5500",
       "http://127.0.0.1:5500",
+      "file:///D:/projects/libraryBookingSystem/Frontend/LibraryBookingSystem.html",
     ],
     credentials: true,
   })
